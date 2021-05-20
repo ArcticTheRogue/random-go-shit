accfb2096d3ce1c5ac8c77edb4727dcbcbc7c093
ArcticTheRogue <arctictherogue@gmail.com>
2021-05-20 16:50:09 -0400

    I think I'm closer

--- bubblesort/bubblesort.go
+++ bubblesort/bubblesort.go
@@ -6,4 +6,14 @@ func main() {
 	fmt.Println("Hello, world!")
 	numbers := [5]int{5, 1, 4, 2, 8}
 	fmt.Println("Numbers before sort:", numbers)
+	var a = 0
+	var b = 1
+	fmt.Println(numbers[a], numbers[b])
+	if a < 3 && b < 4 {
+		if numbers[a] > numbers[b] {
+			numbers[a], numbers[b] = numbers[b], numbers[a]
+			fmt.Println(numbers)
+			return
+		}
+	}
 }
