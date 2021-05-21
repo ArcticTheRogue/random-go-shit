ccb3eae4d4e7b68f5b39aa00691bfa568eae07b5
ArcticTheRogue <arctictherogue@gmail.com>
2021-05-21 09:37:40 -0400

    Add comments

--- bubblesort/bubblesort.go
+++ bubblesort/bubblesort.go
@@ -4,18 +4,25 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, world!")
+	// set numbers
 	numbers := [5]int{5, 1, 4, 2, 8}
 	fmt.Println("Numbers before sort:", numbers)
+	// set vars for first set of numbers
 	var a = 0
 	var b = 1
 	fmt.Println(numbers[a], numbers[b])
+	// set for loop for if set does is less or equal to the last set
 	for a <= 3 && b <= 4 {
+		// if statement for if the first number is larger than the second
 		if numbers[a] > numbers[b] {
+			//swap numbers
 			numbers[a], numbers[b] = numbers[b], numbers[a]
 			fmt.Println(numbers)
+			// add one to each var so it moves to the next set
 			a += 1
 			b += 1
 		} else {
+			// same as end of other if statement
 			a += 1
 			a += 1
 		}
