67d246573b1fa37bcc357b19e740c4ac6e3015f6
ArcticTheRogue <arctictherogue@gmail.com>
2021-05-21 11:54:21 -0400

    Redo comments

--- bubblesort/bubblesort.go
+++ bubblesort/bubblesort.go
@@ -11,10 +11,15 @@ func main() {
 }
 
 func sort(numbers []int) {
+	// var for length of array
 	arrl := len(numbers) - 1
+	// set a var; while a is less than length of array; add 1 to a
 	for a := 0; a < arrl; a++ {
+		// set b var; while b is less than array length minus a; add 1 to b
 		for b := 0; b < arrl-a; b++ {
+			// if first in set is larger than second in set
 			if numbers[b] > numbers[b+1] {
+				// switch sets
 				numbers[b], numbers[b+1] = numbers[b+1], numbers[b]
 			}
 		}
