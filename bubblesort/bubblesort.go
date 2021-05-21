65fdaebf6ec460fd92339fbdbd8a4bac10e3235f
ArcticTheRogue <arctictherogue@gmail.com>
2021-05-20 22:52:44 -0400

    Closer maybe?

--- bubblesort/bubblesort.go
+++ bubblesort/bubblesort.go
@@ -9,12 +9,15 @@ func main() {
 	var a = 0
 	var b = 1
 	fmt.Println(numbers[a], numbers[b])
-	for a < 3 && b < 4 {
+	for a <= 3 && b <= 4 {
 		if numbers[a] > numbers[b] {
 			numbers[a], numbers[b] = numbers[b], numbers[a]
 			fmt.Println(numbers)
 			a += 1
 			b += 1
+		} else {
+			a += 1
+			a += 1
 		}
 	}
 }
