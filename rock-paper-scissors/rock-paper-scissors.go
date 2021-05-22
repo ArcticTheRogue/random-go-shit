15c43d496d15e6561ec491bbd75b65b219f2beff
ArcticTheRogue <arctictherogue@gmail.com>
2021-05-22 15:15:43 -0400

    slightly farther in

--- rock-paper-scissors/rock-paper-scissors.go
+++ rock-paper-scissors/rock-paper-scissors.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello")
+	rand.Seed(time.Now().Unix())
+	var name string
+	var a = rand.Intn(2)
+	fmt.Println("Welcome to rock, paper, scissor!")
+	options := [3]string{"rock", "paper", "scissor"}
+	fmt.Println("The options are:", options)
+	fmt.Printf("Please choose an option: ")
+	fmt.Scanf("%s", &name)
+	fmt.Println("You chose: ", name)
+	var b = options[a]
+	switch name {
+	case b:
+		fmt.Println("Tie")
+	}
 }
