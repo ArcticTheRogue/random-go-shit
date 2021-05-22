af24b667371ada956b1602af042634034e06e022
ArcticTheRogue <arctictherogue@gmail.com>
2021-05-22 17:23:10 -0400

    Finally done

--- rock-paper-scissors/rock-paper-scissors.go
+++ rock-paper-scissors/rock-paper-scissors.go
@@ -7,27 +7,58 @@ import (
 )
 
 func main() {
-	rand.Seed(time.Now().Unix())
 	var name string
-	var a = rand.Intn(2)
 	fmt.Println("Welcome to rock, paper, scissor!")
 	options := [3]string{"rock", "paper", "scissor"}
 	fmt.Println("The options are:", options)
 	fmt.Printf("Please choose an option: ")
 	fmt.Scanf("%s", &name)
 	fmt.Println("You chose: ", name)
-	convert(name, options[:])
-}
-
-func convert(name string, options []string) {
 	switch name {
 	case options[0]:
 		c := 0
+		solve(c)
 	case options[1]:
 		c := 1
+		solve(c)
 	case options[2]:
 		c := 2
+		solve(c)
 	default:
 		fmt.Println("Not a valid option")
+	}
+}
+
+func solve(c int) {
+	rand.Seed(time.Now().Unix())
+	var a = rand.Intn(2)
+	switch {
+	case c == a:
+		fmt.Println("Tie")
+	default:
+		switch c == 0 {
+		case a == 2:
+			fmt.Println("Bot chooses", a)
+			fmt.Println("You win")
+		case a == 1:
+			fmt.Println("Bot chooses", a)
+			fmt.Println("You lose")
+		}
+		switch c == 1 {
+		case a == 0:
+			fmt.Println("Bot chooses", a)
+			fmt.Println("You win")
+		case a == 2:
+			fmt.Println("Bot chooses", a)
+			fmt.Println("You lose")
+		}
+		switch c == 2 {
+		case a == 1:
+			fmt.Println("Bot chooses", a)
+			fmt.Println("You win")
+		case a == 0:
+			fmt.Println("Bot chooses", a)
+			fmt.Println("You lose")
+		}
 	}
 }
